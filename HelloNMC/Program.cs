7553f67005fe99cc578d84e55faca434ac047164
Christine Largent <largenc@ad.nmc.edu>
2019-04-01 14:05:45 -0400

    Add project files.

---
+++ HelloNMC/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloNMC
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine();
+            Console.WriteLine("Hello NMC!");
+            Console.WriteLine();
+            Console.WriteLine("Please press any read to exit");
+            Console.ReadKey();
+        }
+    }
+}
